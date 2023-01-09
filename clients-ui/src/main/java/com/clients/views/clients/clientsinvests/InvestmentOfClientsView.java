ed15511302dcd074ac105c327ba201e55f7896e6
ConstantinTim <constantin.raducu999@gmail.com>
2023-01-09 08:03:43 +0100

    Notification

--- clients-ui/src/main/java/com/clients/views/clients/clientsinvests/InvestmentOfClientsView.java
+++ clients-ui/src/main/java/com/clients/views/clients/clientsinvests/InvestmentOfClientsView.java
@@ -11,9 +11,11 @@ import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.*;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
@@ -21,8 +23,8 @@ import com.vaadin.flow.data.provider.Query;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.ui.Dependency;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -80,16 +82,19 @@ public class InvestmentOfClientsView extends VerticalLayout {
 
         grid.addComponentColumn(investmentsOfClientsDto -> {
             Button button = new Button("Activate");
-            if (investmentsOfClientsDto.getStatusOfPayment()) {
-                button.setVisible(false);
-            } else {
-                button.setAutofocus(true);
-                button.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
-                button.addClickListener(event -> {
-                    clientsInvestmentService.activateExpiredInvestment(investmentsOfClientsDto);
-                    updateList();
-                });
-            }
+            button.setAutofocus(true);
+            button.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+            button.addClickListener(event -> {
+                clientsInvestmentService.activateExpiredInvestment(investmentsOfClientsDto);
+                updateList();
+                String text = "Investment " + investmentsOfClientsDto.getInvestmentName() + " made by " + investmentsOfClientsDto.getFirstName() + "  " + investmentsOfClientsDto.getLastName() + " updated successfully!";
+                Notification notification = new Notification(text);
+                notification.setDuration(2000);
+                notification.setPosition(Position.BOTTOM_STRETCH);
+                notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+                notification.open();
+            });
+
             return button;
         }).setHeader("Action");
     }
@@ -160,15 +165,16 @@ public class InvestmentOfClientsView extends VerticalLayout {
         setSearchingEvent();
     }
 
-    private void setSearchingEvent(){
+    private void setSearchingEvent() {
         this.firstName.addValueChangeListener(event -> searchInvestments());
         this.lastName.addValueChangeListener(event -> searchInvestments());
         this.investmentName.addValueChangeListener(event -> searchInvestments());
         this.status.addValueChangeListener(event -> searchInvestments());
     }
+
     private void searchInvestments() {
         List<InvestmentsOfClientsDto> investmentsSearch = clientsInvestmentService.searchInvestments(
-                new SearchInvestmentDto(firstName.getValue(),lastName.getValue(),status.getValue(),investmentName.getValue()));
+                new SearchInvestmentDto(firstName.getValue(), lastName.getValue(), status.getValue(), investmentName.getValue()));
 
         grid.setItems(investmentsSearch);
     }
